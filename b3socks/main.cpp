528d8e53f8098aeb10cc25c5f92a943c2a31ba33
Bahadir <bahadir@bahadr.me>
2014-12-12 21:02:18 +0200

    Initial Commit

---
+++ b3socks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  b3socks
+//
+//  Created by Bahadir on 12/12/14.
+//  Copyright (c) 2014 bahadir.me. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
